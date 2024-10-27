6f41130805b754fda84d7ee3fea970d82262243e
jaehoonmandev <wido1593@gmail.com>
2024-10-27 21:55:40 +0900

    webhook 테스트

--- src/main/java/org/jaehoonman/jenkins_sample/controller/home.java
+++ src/main/java/org/jaehoonman/jenkins_sample/controller/home.java
@@ -1,13 +1,31 @@
 package org.jaehoonman.jenkins_sample.controller;
 
+import org.jaehoonman.jenkins_sample.entity.Users;
+import org.jaehoonman.jenkins_sample.service.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class home {
 
+    private UsersService usersService;
+
+    @Autowired
+    public void setUsersService(UsersService usersService){
+        this.usersService = usersService;
+    }
+
     @GetMapping("/")
-    public String home(){
+    public String home(Model model){
+
+        List<Users> users = usersService.findAllUsers();
+
+        model.addAttribute("users", users);
+
         return "home";
     }
 }
